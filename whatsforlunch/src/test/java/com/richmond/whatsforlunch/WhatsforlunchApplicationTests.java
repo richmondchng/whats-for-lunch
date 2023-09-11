89535bd11aa37e03d84bb29b8b353cb622bfa75d
richmondchng <richmondchng@gmail.com>
2023-09-12 01:51:02 +0800

    initial commit for spring boot

---
+++ whatsforlunch/src/test/java/com/richmond/whatsforlunch/WhatsforlunchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.richmond.whatsforlunch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsforlunchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

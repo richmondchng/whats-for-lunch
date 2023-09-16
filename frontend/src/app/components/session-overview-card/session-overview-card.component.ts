5cc7564dcb5ce4eab89dc8f7d688e5d390446c6c
richmondchng <richmondchng@gmail.com>
2023-09-17 01:19:26 +0800

    add border to indicate user is the owner of session

--- frontend/src/app/components/session-overview-card/session-overview-card.component.ts
+++ frontend/src/app/components/session-overview-card/session-overview-card.component.ts
@@ -1,17 +1,27 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { SessionDetails } from 'src/app/interfaces/SessionDetails';
-
+import { Me } from 'src/app/interfaces/Me';
 
 @Component({
   selector: 'app-session-overview-card',
   templateUrl: './session-overview-card.component.html',
   styleUrls: ['./session-overview-card.component.css']
 })
-export class SessionOverviewCardComponent {
+export class SessionOverviewCardComponent implements OnInit {
+
   @Input() session!: SessionDetails;
   @Output() onDeleteSession: EventEmitter<SessionDetails> = new EventEmitter();
   faTimes = faTimes;
+  sessionOwner: boolean = false;
+
+  ngOnInit(): void {
+    const obj = localStorage.getItem("me")
+    if(obj) {
+      const me : Me = JSON.parse(obj);
+      this.sessionOwner = (this.session.ownerId ===  me.userId);
+    }
+  }
 
   onDelete(session:SessionDetails) {
     if(confirm("Are you sure you want to delete this session?")) {

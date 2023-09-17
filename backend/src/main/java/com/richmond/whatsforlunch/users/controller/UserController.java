aca5ae244cdbd8e1488faea45cdc68d3d5e686d4
richmondchng <richmondchng@gmail.com>
2023-09-17 23:31:59 +0800

    update api to have option to include admin user

--- backend/src/main/java/com/richmond/whatsforlunch/users/controller/UserController.java
+++ backend/src/main/java/com/richmond/whatsforlunch/users/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,8 +25,8 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<StandardResponse<ResponseUser>> getUsers() {
-        return ResponseEntity.ok(new StandardResponse<>(mapToBeans(userService.getAllUsers())));
+    public ResponseEntity<StandardResponse<ResponseUser>> getUsers(@RequestParam(defaultValue = "false") final boolean includeAdmin) {
+        return ResponseEntity.ok(new StandardResponse<>(mapToBeans(userService.getAllUsers(includeAdmin))));
     }
 
     @GetMapping(value = "/{userName}", produces = MediaType.APPLICATION_JSON_VALUE)
